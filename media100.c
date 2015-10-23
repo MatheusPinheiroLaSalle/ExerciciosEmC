9c15b8a7423849cda203d2bc5ed4a1f795d4c83b
matheuspinheiro <matheuspinheiro.lasalle@gmail.com>
2015-10-23 23:13:34 +0000

    Exercicios em C

---
+++ media100.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=0,media;
+    
+    while(num <= 100)
+    
+    printf("%d - ", num++);
+    
+    media = (101*50)/2;
+       
+    printf("\n %d - ", media);
+    
+    
+    return 0;
+}
